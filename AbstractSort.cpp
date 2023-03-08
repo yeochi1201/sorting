d0723433adf3012e7b1128df6c883a1abd9160fe
박주현 <yeochi1201@gmail.com>
2023-03-08 20:57:34 +0900

    1.0
    abstract sort class create

---
+++ AbstractSort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+template <class Comparable>
+class AbstractSort{
+public:
+	public size(Comparable[] a) {
+		return sizeof(a) / sizeof(Comparable);
+	}
+
+	int compareTo(Comparable a, Comparable b) {
+		if (a < b)return -1;
+		else if (a == b)return 0;
+		else return 1;
+	}
+
+	bool less(Comparable a, Comparable b) {
+		return compareTo(a,b) < 0;
+	}
+	
+	void exch(Comparable[] a, int i, int j) {
+		Comparable t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+
+	void show(Comparable[] a) {
+		for (int i = 0; i < size(a); i++) {
+			cout << a[i] << "  ";
+		}
+		cout << endl;
+	}
+
+	bool isSorted(Comparable[] a) {
+		for (int i = 1; i < size(a); i++) {
+			if (less(a[i], a[i - 1]))return false;
+		}
+	}
+};
